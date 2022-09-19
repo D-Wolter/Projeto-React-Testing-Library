4045961e908bb08d0216a21979ce78ae030f8f6e
Daniel Wolter <wolter.daniel@gmail.com>
2022-09-19 19:51:28 -0300

    req 3 all kills

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -4,8 +4,8 @@ import renderWithRouter from '../renderWithRouter';
 
 import FavoritePokemons from '../pages/FavoritePokemons';
 
-describe('Teste links de navegação: about', () => {
-  test('Teste se link de navegação: about é renderizado', () => {
+describe('Teste links de navegação: FavoritePokemons', () => {
+  test('Teste se link de navegação: FavoritePokemons é renderizado', () => {
     const { history } = renderWithRouter(<FavoritePokemons />);
     const { location: { pathname } } = history;
     expect(pathname).toBe('/');
