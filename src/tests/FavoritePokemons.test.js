051e6d67b39a30c49f0488cefcab86a232fe2e6a
Daniel Wolter <wolter.daniel@gmail.com>
2022-09-19 19:34:33 -0300

    req 3 all kill

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Teste links de navegação: about', () => {
+  test('Teste se link de navegação: about é renderizado', () => {
+    const { history } = renderWithRouter(<FavoritePokemons />);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+
+    const found = screen.getByText(/no favorite pokemon found/i);
+    expect(found).toBeInTheDocument();
+  });
+});
