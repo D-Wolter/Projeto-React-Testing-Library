d3700e4391926c166e82372d88d3fde72e5dc37d
Daniel Wolter <wolter.daniel@gmail.com>
2022-09-20 18:00:29 -0300

    req 5 dev

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,66 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+
+import App from '../App';
+import data from '../data';
+
+const checkNextPokemon = (filterBtn, arrayPok) => {
+  arrayPok.forEach((elem, idx, array) => {
+    const last = array.length - 1;
+    if (idx !== last) {
+      const pok = screen.getByText(elem.name);
+      expect(pok).toBeInTheDocument();
+      userEvent.click(filterBtn);
+      const nextElem = array[idx + 1];
+      const nextPok = screen.getByText(nextElem.name);
+      expect(nextPok).toBeInTheDocument();
+    } else {
+      const pok = screen.getByText(elem.name);
+      expect(pok).toBeInTheDocument();
+      userEvent.click(filterBtn);
+      const nextElem = array[0];
+      const nextPok = screen.getByText(nextElem.name);
+      expect(nextPok).toBeInTheDocument();
+    }
+  });
+};
+
+describe('Teste componente Pokemons', () => {
+  test('Teste se a página contém um heading h2 com o texto Encountered pokémons', () => {
+    const { history } = renderWithRouter(<App />);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+
+    const encountered = screen.getByRole('heading', {
+      name: /encountered pokémons/i,
+      level: 2,
+    });
+    expect(encountered).toBeInTheDocument();
+  });
+  test('Teste se percorre os elementos array ao clicar em proximo', () => {
+    const { history } = renderWithRouter(<App />);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+
+    const nextPokButton = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    expect(nextPokButton).toBeInTheDocument();
+    checkNextPokemon(nextPokButton, data);
+  });
+  test('Teste botao resetar filtro', () => {
+    const { history } = renderWithRouter(<App />);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/');
+
+    const nextPokButton = screen.getByRole('button', {
+      name: /próximo pokémon/i,
+    });
+    const clearButton = screen.getByRole('button', { name: /all/i });
+    expect(clearButton).toBeInTheDocument();
+    userEvent.click(clearButton);
+    checkNextPokemon(nextPokButton, data);
+  });
+});
