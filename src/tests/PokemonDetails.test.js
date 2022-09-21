21a033781cd4da3bdd377cccef4418c9d44fabb2
Daniel Wolter <wolter.daniel@gmail.com>
2022-09-21 11:41:31 -0300

    req 6 all kill

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -3,6 +3,7 @@ import { screen } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
+import data from '../data';
 
 describe('teste PokemonDetails', () => {
   test('teste se as infos detalhadas do pokémon selecionado são exibidas:', () => {
@@ -20,5 +21,28 @@ describe('teste PokemonDetails', () => {
     const txt2 = 'with electricity to make them tender enough to eat.';
     const fullTxt = screen.getByText(txt1 + txt2);
     expect(fullTxt).toBeInTheDocument();
+  });
+  test('mapas e localizaçoes', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/pokemons/25');
+    });
+    const location = screen.getByRole('heading', { name: /locations/i });
+    expect(location).toBeInTheDocument();
+    const pikachuInfo = data[0];
+    const { foundAt } = pikachuInfo;
+    foundAt.forEach((info) => {
+      const locationTxt = screen.getByText(info.location);
+      expect(locationTxt).toBeInTheDocument();
+    });
+    const locationImg = screen.getAllByRole('img', { name: 'Pikachu location' });
+    locationImg.forEach((item) => {
+      const checkImg = foundAt.some(({ map }) => map === item.src);
+      expect(checkImg).toBeTruthy();
+    });
+    const allLocations = locationImg.length;
+    expect(allLocations).toBe(foundAt.length);
+    const favorite = screen.getByText(/pokémon favoritado\?/i);
+    expect(favorite).toBeInTheDocument();
   });
 });
