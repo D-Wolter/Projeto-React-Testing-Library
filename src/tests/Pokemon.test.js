fc1813fe261421d5a9099d2d304f089cb7a12095
Daniel Wolter <wolter.daniel@gmail.com>
2022-09-21 18:06:10 -0300

    req 7 all kill

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 
-describe('component pokémon:', () => {
+describe('Test component pokémon:', () => {
   test('whether a card is rendered with information from a particular Pokémon:', () => {
     renderWithRouter(<App />);
     const pokName = screen.getByTestId('pokemon-name');
@@ -17,5 +19,26 @@ describe('component pokémon:', () => {
     expect(pokWeight.innerHTML).toBe('Average weight: 6.0 kg');
     const pokImg = screen.getByRole('img', { name: /Pikachu sprite/i });
     expect(pokImg.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+  test(`Test whether the Pokémon card listed on Pokédex contains a navigation link to
+  display details of this Pokémon. The link must have the URL /pokemons/, where is the
+  pokémon id displayed`, () => {
+    const { history } = renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(moreDetails);
+    const { location: { pathname } } = history;
+    expect(pathname).toBe('/pokemons/25');
+  });
+  test('if there is a star icon in favorite Pokémon:', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/pokemons/25');
+    });
+    const checkBox = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(checkBox);
+    const pokIcon = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(pokIcon).toBeInTheDocument();
+    expect(pokIcon.src.endsWith('/star-icon.svg')).toBeTruthy();
+    expect(pokIcon.alt).toBe('Pikachu is marked as favorite');
   });
 });
